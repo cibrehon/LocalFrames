291fe054ec298c9b80639e6654b8b4fa20fb1160
Ege <ege@Eges-Mac-mini.local>
2019-05-31 20:46:53 +0200

    Dynamic Navigation is added.

---
+++ navigation.js
@@ -0,0 +1,18 @@
+
+var m_menuEntries = ["EGE ILICAK - STREET PHOTOGRAPHER <br>","Portfolio", "About", "Contact", "Prints"];
+var m_menuLinks =   ["main.html","portfolio.html", "about.html", "contact.html", "prints.html"];
+
+
+// Create links automatically.
+for (let index = 0; index < m_menuEntries.length; index++) 
+{
+    var menuEntry = m_menuEntries[index];
+    var menuLink = m_menuLinks[index];
+
+    var finalLink = document.createElement("A");
+    finalLink.innerHTML = menuEntry + "<br>";
+    finalLink.href = menuLink;
+    document.body.appendChild(finalLink);   
+}
+
+
