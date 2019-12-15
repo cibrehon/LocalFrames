d299f8f9aee3a8ffc02d055fdc0bcfcc34b959aa
Ege <ege@Eges-Mac-mini.local>
2019-12-15 18:42:24 +0100

    Curtain Menu implementation

--- Controller/navigation.js
+++ Controller/navigation.js
@@ -1,13 +1,5 @@
-
-var m_menuEntries = ["EGE ILICAK | Street & Travel Photography <br>","+Portfolio", "<br> Prints", "About Me"];
-var m_menuLinks =   ["index.html","portfolio.html", "prints.html", "about.html"];
-
-var m_galleryEntires = ["Stories of Japan","Faces of Cuba", "Russia"];
-var m_galleryLinks = ["japan.html","cuba.html","russia.html"];
-
-var m_galleryMenu = document.createElement("div");
-m_galleryMenu.id = "galleryMenu";
-m_galleryMenu.className = "dropdown";
+var m_menuEntries = ["Stories of Japan", "Faces of Cuba", "<br> Prints", "About Me"];
+var m_menuLinks =   ["japan.html", "cuba.html", "prints.html", "about.html"];
 
 
 CreateMenuLinks();
@@ -22,46 +14,13 @@ function CreateMenuLinks()
         var menuLink = m_menuLinks[index];
         var finalLink; 
 
-        if (menuEntry == "+Portfolio") 
-        {
-            finalLink = document.createElement("button");
-            finalLink.id = menuEntry;
-            finalLink.innerHTML = menuEntry + "<br>";
-            CreateSubGalleries();
-            document.getElementById("menu").appendChild(finalLink);
-            document.getElementById("menu").appendChild(m_galleryMenu);
-        } 
-        else 
-        {
-            finalLink = document.createElement("A");    
-            finalLink.id = menuEntry;
-            finalLink.innerHTML = menuEntry + "<br>";
-            finalLink.href = menuLink;
-            document.getElementById("menu").appendChild(finalLink);
-        }
-
+        finalLink = document.createElement("A");    
+        finalLink.id = menuEntry;
+        finalLink.innerHTML = menuEntry + "<br>";
+        finalLink.href = menuLink;
+        document.getElementById("MainMenu").appendChild(finalLink);
     }
 }
-
-function CreateSubGalleries() 
-{
-    //Create sub entires of portfolio.
-    for (let index = 0; index < m_galleryEntires.length; index++) 
-    {
-        var galleryEntry = m_galleryEntires[index];
-        var galleryLink = m_galleryLinks[index];
-       
-
-        var finalGalleryLink = document.createElement("A");    
-        finalGalleryLink.id = galleryEntry;
-        finalGalleryLink.innerHTML = galleryEntry + "<br>";
-        finalGalleryLink.href = galleryLink;
-
-        m_galleryMenu.appendChild(finalGalleryLink);
-    }
-   
-}
-
 function CreateSocialMediaIcons()
 {
     var instagramLink = document.createElement("A");
@@ -69,24 +28,16 @@ function CreateSocialMediaIcons()
     instagramLink.innerHTML = "<img class='social'; src='Resources/Social/instagram.png'>";
     instagramLink.href = "https://www.instagram.com/local.frames/";
     instagramLink.target="_blank";
-    document.getElementById("menu").appendChild(instagramLink);
+    document.getElementById("MainMenu").appendChild(instagramLink);
 
-    
 }
 
-// Get the button, and when the user clicks on it, execute showAndHide
-document.getElementById("+Portfolio").onclick = function() {showAndHide()};
-
-/* showAndHide toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function showAndHide() 
+function openNav() 
 {
-    if(m_galleryMenu.style.display == "block")
-    {
-        m_galleryMenu.style.display = "none";
-    }
-    else
-    {
-        m_galleryMenu.style.display = "block";
-    }
+    document.getElementById("CurtainMenu").style.width = "100%";
 }
-
+  
+function closeNav() 
+{
+    document.getElementById("CurtainMenu").style.width = "0%";
+}
