0f5bf8b546a0f0a9035481b4131c66c60a53499b
cibrehon <egeilicak@hotmail.com>
2021-02-14 22:59:24 +0100

    Completely New Responsive portfiolio pages

--- Controller/slider.js
+++ Controller/slider.js
@@ -1,5 +1,6 @@
 var m_currentIndexN = 0;
 var m_prevIndex = 0;
+
 m_totalImageCount = 0;
 
 function SlideByKey()
@@ -12,7 +13,8 @@ function SlideByKey()
 
 function SetNextIndex(dir)
 {
-  ShowandHide(dir);
+  //ShowandHide(dir);
+  ChangeImage(dir);
 }
 
 function SetNextIndexbyPosition(e)
@@ -23,9 +25,69 @@ function SetNextIndexbyPosition(e)
    
   xCoordinate = e.clientX;
   var direction = FindPosition(xCoordinate);
-  ShowandHide(direction);
+ // ShowandHide(direction);
+  ChangeImage (direction);
 }
 
+function ChangeImage(dir)
+{
+  
+  var direction = dir;
+  var imageCounter = document.getElementById("IC");
+  var pName = document.getElementById("currentImage").alt;
+
+  m_currentIndexN += direction; 
+
+  if(m_currentIndexN >= m_totalImageCount)
+  {
+    m_currentIndexN = 0;
+  }
+  
+  if(m_currentIndexN < 0)
+  {
+    m_currentIndexN = m_totalImageCount - 1;
+  }
+
+  imageCounter.innerText =  m_currentIndexN + 1 + " / " + m_totalImageCount;
+
+  if (pName=="russia")
+  {
+    document.getElementById("currentImage").src=`Resources/Photos/Russia/${m_currentIndexN+1}.jpg`;
+  }
+  else if (pName=="japan")
+  {
+    document.getElementById("currentImage").src=`Resources/Photos/Japan/${m_currentIndexN+1}.jpg`;
+  }
+  else if (pName=="cuba")
+  {
+    document.getElementById("currentImage").src=`Resources/Photos/Cuba/${m_currentIndexN+1}.jpg`;
+  }
+}
+
+// Checks the mouse position on the x-axis. Returns 1 if mouse is on the right side, returns -1 for the left side
+function FindPosition(x)
+{
+  var size = document.body.clientWidth;
+  if( x >= (size /2)){ return 1;} else { return -1;}
+}
+
+function SetTotalImageCount(imageCount)
+{
+  m_totalImageCount = imageCount;
+}
+
+/*
+function StartAutoSlideShow()
+{
+ // PlusSlides(1);
+ SetNextIndex(1);
+ setTimeout(StartAutoSlideShow, 3000);
+
+}
+*/
+
+
+/*
 function ShowandHide(dir)
 {
   var direction = dir;
@@ -54,23 +116,4 @@ function ShowandHide(dir)
   slideList[m_prevIndex].classList.remove("show");
   slideList[m_prevIndex].classList.add("hide");
 }
-
-// Checks the mouse position on the x-axis. Returns 1 if mouse is on the right side, returns -1 for the left side
-function FindPosition(x)
-{
-  var size = document.body.clientWidth;
-  if( x >= (size /2)){ return 1;} else { return -1;}
-}
-
-function SetTotalImageCount(imageCount)
-{
-  m_totalImageCount = imageCount;
-}
-
-function StartAutoSlideShow()
-{
- // PlusSlides(1);
- SetNextIndex(1);
- setTimeout(StartAutoSlideShow, 3000);
-
-}
+*/
